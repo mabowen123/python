f5466d3c6f05be9a06628a6a9fae73541846a12d
mabowen123 <610286632@qq.com>
2018-09-30 09:29:44 +0800

    猜数字游戏

--- guessingNumberGame.py
+++ guessingNumberGame.py
@@ -3,30 +3,36 @@
 # @Author :mabowen
 # @Time   :2018/9/29 16:28
 import random
+"""
+生成一个随机数,猜中数字
+"""
 
 def game():
-    userNum = input('请输入你猜得数字1到10000:')
-    userNum = int(userNum)
-    count = 0
+    count = 1
+    userNum = userIput()
     while 1:
         if userNum == num:
             print('恭喜你猜中了')
             print('一共用了' + str(count) + '次')
             bool = input('任意键退出')
             if bool:
-                print(1)
-                exit()
+                exit('bye')
         elif userNum > num:
             print('大了')
             count += 1
-            game()
+            userNum = userIput()
         elif userNum < num:
             print('小了')
             count += 1
-            game()
+            userNum = userIput()
+
+
+def userIput():
+    userNum = input('请输入你猜得数字1到10000:')
+    userNum = int(userNum)
+    return userNum
 
 
 if __name__ == '__main__':
     num = random.randint(0, 10000)
-    print(num)
     game()
